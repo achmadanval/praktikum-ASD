89ae490f4fdc8000b17d9ded51dd51dcf161ab4b
achmadanval <anvalalan@gmail.com>
2025-02-21 08:59:59 +0700

    hai 2

--- jobsheet 2/Mahasiswa02.java
+++ jobsheet 2/Mahasiswa02.java
@@ -6,17 +6,17 @@ public class Mahasiswa02 {
     double ipk;     
 
 
-    public static void tampilkanInformasi() {
+     void tampilkanInformasi() {
         System.out.println("NAMA : " + nama );
         System.out.println("NIM :" + nim);
         System.out.println("IPK :" + ipk);
         System.out.println("KELAS :" + kelas);
     }
 
-    public static void ubahKelas(String kelasBaru) {
+     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
-    static void updateIPK(double ipkBaru){
+     void updateIPK(double ipkBaru){
         if (ipkBaru <= 0.0 || ipkBaru >= 4.0) {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0 ");
         }else{
