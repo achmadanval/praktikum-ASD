84927464d4f5d75a4cbfc3ec24ef262fba8ff1d8
achmadanval <anvalalan@gmail.com>
2025-02-28 13:48:33 +0700

    PERTANYAAN 1

--- jobsheet 3/Mahasiswa02.java
+++ jobsheet 3/Mahasiswa02.java
@@ -4,4 +4,10 @@ public class Mahasiswa02 {
     public String nama;
     public String kelas;
     public float ipk ;
+    public void cetakInfo(){
+        System.out.println("NIM : " + nim);
+        System.out.println(" : " + nama);
+        System.out.println("KELAS : " + kelas);
+        System.out.println("IPK : " + ipk);
+}
 }
--- jobsheet 3/MahasiswaDemo02.java
+++ jobsheet 3/MahasiswaDemo02.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
-public class MahasiswaDemo02{
+
+public class MahasiswaDemo02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Mahasiswa02[] arrayMahasiswa02 = new Mahasiswa02[3];
-        String dummy;
 
         for (int i = 0; i < 3; i++) {
             arrayMahasiswa02[i] = new Mahasiswa02();
             System.out.println("Masukkan Data Mahasiswa ke- " + (i + 1));
-            System.out.print("NIM : ");
+
+            System.out.print("NIM   : ");
             arrayMahasiswa02[i].nim = sc.nextLine();
-            System.out.print("NAMA : ");
+            System.out.print("NAMA  : ");
             arrayMahasiswa02[i].nama = sc.nextLine();
             System.out.print("KELAS : ");
             arrayMahasiswa02[i].kelas = sc.nextLine();
-            System.out.print("IPK : ");
-            dummy = sc.nextLine();
-            arrayMahasiswa02[i].ipk = Float.parseFloat(dummy);
+            System.out.print("IPK   : ");
+            arrayMahasiswa02[i].ipk = sc.nextFloat();
+            sc.nextLine();
+
             System.out.println("----------------------------------");
         }
+        System.out.println("\n--- DATA MAHASISWA ---");
         for (int i = 0; i < 3; i++) {
-            System.out.println("Masukkan Data Mahasiswa ke- " + (i + 1));
-            System.out.println("NIM   : " + arrayMahasiswa02[i].nim);
-            System.out.println("NAMA  : " + arrayMahasiswa02[i].nama);
-            System.out.println("KELAS : " + arrayMahasiswa02[i].kelas);
-            System.out.println("IPK   : " + arrayMahasiswa02[i].ipk);
+            arrayMahasiswa02[i].cetakInfo();
         }
     }
 }
