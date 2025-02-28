533c5c5e3026656168438f8cdd3b1ede0a0f17c4
achmadanval <anvalalan@gmail.com>
2025-02-28 15:11:05 +0700

    percobaan 3

--- jobsheet 3/MahasiswaDemo02.java
+++ jobsheet 3/MahasiswaDemo02.java
@@ -4,6 +4,11 @@ public class MahasiswaDemo02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Mahasiswa02[] arrayMahasiswa02 = new Mahasiswa02[3];
+        Mahasiswa02[] myArrayOfMahasiswa02 = new Mahasiswa02[3];
+        myArrayOfMahasiswa02[0].nim = "244107060033";
+        myArrayOfMahasiswa02[0].nama = "AGNES TITANIA KINANTI";
+        myArrayOfMahasiswa02[0].kelas = "SIB-1E";
+        myArrayOfMahasiswa02[0].ipk = (float) 3.75;
 
         for (int i = 0; i < 3; i++) {
             arrayMahasiswa02[i] = new Mahasiswa02();
